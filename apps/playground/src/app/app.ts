4a5a3d70916ca012c7ead491b98e36e7aecc56f9
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2026-01-27 21:49:37 +0900

    feat: playground

--- apps/playground/src/app/app.ts
+++ apps/playground/src/app/app.ts
@@ -1,15 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
-import { ToastContainerComponent, ToastService } from 'ngx';
+import { OverlayComponent, ToastContainerComponent, ToastService } from 'ngx';
 
 @Component({
   selector: 'app-root',
-  imports: [ToastContainerComponent],
+  imports: [OverlayComponent, ToastContainerComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   private readonly toastService = inject(ToastService);
+
+  public isOverlayVisibleSignal = signal<boolean>(false);
+
+  public onClickShowOverlayButton(): void {
+    this.isOverlayVisibleSignal.set(true);
+  }
+
+  public onClickHideOverlayButton(): void {
+    this.isOverlayVisibleSignal.set(false);
+  }
 
   public onClickAddErrorToastButton(): void {
     this.toastService.add({
