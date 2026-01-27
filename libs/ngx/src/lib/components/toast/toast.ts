02444a04cccd53d48a044dea717a1e4a0a090691
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2026-01-27 19:56:12 +0900

    feat: toast style

--- libs/ngx/src/lib/components/toast/toast.ts
+++ libs/ngx/src/lib/components/toast/toast.ts
@@ -1,19 +1,40 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroExclamationCircleSolid, heroXMarkSolid } from '@ng-icons/heroicons/solid';
 
 import { ToastService } from '../../services';
 import { Toast } from '../../types';
 
 @Component({
   selector: 'x-toast',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './toast.html',
   styleUrl: './toast.css',
+  providers: [provideIcons({ heroExclamationCircleSolid, heroXMarkSolid })],
 })
 export class ToastComponent {
   private readonly toastService = inject(ToastService);
 
   public readonly toastSignal = input.required<Toast>({
     alias: 'toast',
+  });
+
+  public readonly iconNameSignal = computed(() => {
+    switch (this.toastSignal().type) {
+      case 'ERROR':
+        return 'heroExclamationCircleSolid';
+      default:
+        return null;
+    }
+  });
+  public readonly iconColorSignal = computed(() => {
+    switch (this.toastSignal().type) {
+      case 'ERROR':
+        return '#fb2c36'; // text-red-500
+      default:
+        return null;
+    }
   });
 
   public onClickCloseButton(): void {
