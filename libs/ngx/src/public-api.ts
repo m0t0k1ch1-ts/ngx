06bf19313a2f015b30d3dce68311dd2912bd4204
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2026-02-27 21:10:51 +0900

    feat: ripple directive

--- libs/ngx/src/public-api.ts
+++ libs/ngx/src/public-api.ts
@@ -4,5 +4,6 @@ export {
   ToastComponent,
   ToastContainerComponent,
 } from './lib/components';
+export { RippleDirective } from './lib/directives';
 export { ToastService } from './lib/services';
 export type { Toast, ToastType } from './lib/types';
