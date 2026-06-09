56141d4069b8de450be5f05b7b7245cb4ea19b46
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2026-06-09 20:10:46 +0900

    feat: input text dierctive

---
+++ libs/ngx/src/lib/directives/input-text.ts
@@ -0,0 +1,25 @@
+import { Directive, computed, input } from '@angular/core';
+
+@Directive({
+  selector: '[xInputText]',
+  host: {
+    class: 'block rounded-md border px-3 py-2 transition-colors outline-none focus:ring-1',
+    '[class.border-gray-300]': '!touchedAndInvalidSignal()',
+    '[class.border-red-400]': 'touchedAndInvalidSignal()',
+    '[class.focus:border-blue-400]': '!touchedAndInvalidSignal()',
+    '[class.focus:ring-blue-400]': '!touchedAndInvalidSignal()',
+    '[class.focus:ring-red-400]': 'touchedAndInvalidSignal()',
+  },
+})
+export class InputTextDirective {
+  public readonly invalidSignal = input(false, {
+    alias: 'invalid',
+  });
+  public readonly touchedSignal = input(false, {
+    alias: 'touched',
+  });
+
+  public readonly touchedAndInvalidSignal = computed(
+    () => this.touchedSignal() && this.invalidSignal(),
+  );
+}
