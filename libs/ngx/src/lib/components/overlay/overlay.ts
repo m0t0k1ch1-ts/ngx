87a69e26e26e3b5e1391ce6728e9d406892deb73
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2026-07-30 00:56:25 +0900

    refactor(libs/ngx): overlay

--- libs/ngx/src/lib/components/overlay/overlay.ts
+++ libs/ngx/src/lib/components/overlay/overlay.ts
@@ -1,16 +1,25 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'x-overlay',
   imports: [],
   templateUrl: './overlay.html',
   styleUrl: './overlay.css',
+  host: {
+    '[style]': `{
+      '--resolved-z-index': resolvedZIndexSignal(),
+    }`,
+  },
 })
 export class OverlayComponent {
   public readonly isVisibleSignal = input.required<boolean>({
     alias: 'isVisible',
   });
-  public readonly zIndexSignal = input<number>(50, {
+  public readonly zIndexSignal = input<number | undefined>(undefined, {
     alias: 'zIndex',
+  });
+
+  public readonly resolvedZIndexSignal = computed(() => {
+    return this.zIndexSignal() ?? 'var(--x-overlay-z-index, 50)';
   });
 }
